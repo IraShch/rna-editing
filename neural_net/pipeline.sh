b9a02669fcbbfa69bb0dbd5d57edb350971bd820
IraShch <schukinaira@gmail.com>
2017-02-10 17:55:20 +0000

    pipeline script

---
+++ neural_net/pipeline.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+while getopts ":i:d:o:" opt
+do
+	case "$opt" in
+		i)
+			input_file="$OPTARG";;
+		d)
+			data_dir="$OPTARG";;
+		o)
+			result_dir="$OPTARG";;
+	esac
+done
+
+TAG=$(basename $input_file)
+TAG=${TAG%%.tsv}
+
+python /Users/bioinformaticshub/Documents/Ira/soft/neural_net/rna-editing/neural_net/python/split_data.py -i $input_file -d $data_dir
+python /Users/bioinformaticshub/Documents/Ira/soft/neural_net/rna-editing/neural_net/python/train_test.py -i $TAG -d $data_dir -o $result_dir
+
+curr_dir=$(pwd)
+out_dir=$curr_dir/$result_dir$TAG/train_test_50nodes_400epochs/
+cd $out_dir
+R -e "rmarkdown::render('/Users/bioinformaticshub/Documents/Ira/soft/neural_net/rna-editing/neural_net/R/train_test_report.Rmd', \
+output_file = '"$out_dir$TAG"_report.html', params = list(model_name = '"$TAG"', dir = '"$out_dir"'))"
